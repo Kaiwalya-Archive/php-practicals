46b84dc7b92bb0eff80c6845be42e431c84e458a
Kaiwalya Koparkar <kaiwalyakoparkar@Kaiwalyas-MacBook-Air.local>
2022-05-19 13:17:07 +0530

    switch statement implemented

---
+++ Pract_2/switchStatement.php
@@ -0,0 +1,15 @@
+<?php 
+    $letter='A';
+
+    switch ($letter) {
+        case 'A':
+            echo "Apple \n";
+            break;
+        case 'B':
+            echo "Ball \n";
+            break;
+        default:
+            echo "Guess you didn't find your letter :( ";
+            break;
+    }
+?>
