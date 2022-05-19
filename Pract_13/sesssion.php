e4c64ff2e6850a872abc97e7416d88884fb62b1a
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-05-19 22:45:31 +0530

    session added

---
+++ Pract_13/sesssion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<body>
+
+    <?php 
+
+        //Very first line you should have for session
+        session_start();
+
+    ?>
+
+    <h1>Hello testing out session </h1>
+
+    <!--Setting up the session-->
+    <?php 
+        $_SESSION["color"] = "green";
+        $_SESSION["user"] = "Kaiwalya";
+        echo "Session is set <br>";
+    ?>
+
+    <!--Accessing the sessions-->
+    <?php
+        echo "{$_SESSION["user"]}'s favourite color is {$_SESSION["color"]} <br>";
+    ?>
+
+</body>
+
+</html>
