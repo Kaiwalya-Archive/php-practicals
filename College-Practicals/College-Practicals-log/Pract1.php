9d941f886cc4a8ab900d6e555fd3cfc2d251b531
Kaiwalya Koparkar <kaiwalyakoparkar@Kaiwalyas-MacBook-Air.local>
2022-05-19 13:02:55 +0530

    Completed first practical

--- College-Practicals/College-Practicals-log/Pract1.php
+++ College-Practicals/College-Practicals-log/Pract1.php
@@ -1,4 +1,3 @@
 <?php
-
-echo "Hello PHP";
+    echo "Hello PHP";
 ?>
---
+++ Pract_1/expressionsAndOperators.php
@@ -0,0 +1,12 @@
+<?php 
+    $n1=10;
+    $n2=20;
+
+    echo "Addition is ".$n1+$n2."\n";
+    echo "Subtraction is ".$n1-$n2."\n";
+    echo "Multiplicaiton is ".$n1*$n2."\n";
+    echo "Division is ".$n1/$n2."\n";
+    echo "Modulus is ".$n1%$n2."\n";
+    echo "Exponential is ".$n1**$n2."\n";
+
+?>
