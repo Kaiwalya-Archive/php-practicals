27ef322ce61676384032545773f3e5bbd8dd4608
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2022-05-19 14:52:58 +0530

    simple and parameterized function implemented

---
+++ Pract_6/function.php
@@ -0,0 +1,14 @@
+<?php 
+
+    function hello () {
+        echo "This is hello"."\n";
+    }
+
+    function helloWithName ($name) {
+        echo "Hello this is ".$name."\n";
+    }
+
+    hello();
+    helloWithName ("Kaiwalya");
+
+?>
